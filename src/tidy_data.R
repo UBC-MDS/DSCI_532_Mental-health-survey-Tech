8caa7725cf37ab634ca1f9d2f1fbc03fefbee353
Ting Pan <tingpan-18dsba@outlook.com>
2019-01-21 18:02:45 -0800

    change trans to other

--- src/tidy_data.R
+++ src/tidy_data.R
@@ -21,13 +21,14 @@ data <- data %>% filter (tech_company == 'Yes')
 
 # clean 'Gender'
 data$Gender %<>% str_to_lower()
+
 unique(data$Gender)
 male_str <- c("male", "m", "male-ish", "maile", "mal", "male (cis)", "make", "male ", "man","msle", "mail", "malr", "cis man", "cis male")
-trans_str <- c("trans-female", "something kinda male?", "queer/she/they", "non-binary","nah", "all", "enby", "fluid", "genderqueer", "androgyne", "agender", "male leaning androgynous", "guy (-ish) ^_^", "trans woman", "neuter", "female (trans)", "queer", "ostensibly male, unsure what that really means" )
+other_str <- c("trans-female", "something kinda male?", "queer/she/they", "non-binary","nah", "all", "enby", "fluid", "genderqueer", "androgyne", "agender", "male leaning androgynous", "guy (-ish) ^_^", "trans woman", "neuter", "female (trans)", "queer", "ostensibly male, unsure what that really means" )
 female_str <- c("cis female", "f", "female", "woman",  "femake", "female ","cis-female/femme", "female (cis)", "femail")
-data$Gender <- sapply(as.vector(data$Gender), function(x) if(x %in% male_str) "male" else x )
-data$Gender <- sapply(as.vector(data$Gender), function(x) if(x %in% female_str) "female" else x )
-data$Gender <- sapply(as.vector(data$Gender), function(x) if(x %in% trans_str) "trans" else x )
+data$Gender <- sapply(as.vector(data$Gender), function(x) if(x %in% male_str) "Male" else x )
+data$Gender <- sapply(as.vector(data$Gender), function(x) if(x %in% female_str) "Female" else x )
+data$Gender <- sapply(as.vector(data$Gender), function(x) if(x %in% other_str) "Other" else x )
 data %<>% filter(Gender != "a little about you")
 data %<>% filter(Gender != "guy (-ish) ^_^")
 data %<>% filter(Gender != "p")
